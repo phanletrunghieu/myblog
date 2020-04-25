486189412de9f0fcdb4f53177e931464776714c2
Hieu Phan <hieutrunglephan@gmail.com>
2020-04-26 01:13:08 +0700

    update og:image url

--- components/Meta.js
+++ components/Meta.js
@@ -1,5 +1,10 @@
 import Head from 'next/head'
+import siteData from '../data/config.json'
 export default function Meta({title, description, image}) {
+    let img = image || "http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
+    if (img.indexOf("http://") == -1 && img.indexOf("https://") == -1){
+        img = siteData.url + img
+    }
     return (
         <Head>
             <title>{title}</title>
@@ -9,7 +14,7 @@ export default function Meta({title, description, image}) {
             <meta property="og:type"        content="article" />
             <meta property="og:title"       content={title} />
             <meta property="og:description" content={description} />
-            <meta property="og:image"       content={image || "http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"} />
+            <meta property="og:image"       content={img} />
         </Head>
     )
 }
