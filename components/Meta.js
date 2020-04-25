e1b8f8bc34626900e821fb21273af386a738c889
Hieu Phan <hieutrunglephan@gmail.com>
2020-04-26 01:04:41 +0700

    add og

--- components/Meta.js
+++ components/Meta.js
@@ -5,11 +5,11 @@ export default function Meta({title, description, image}) {
             <title>{title}</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <meta charSet="utf-8" />
-            {/* <meta name="description"        content={description}></meta>
+            <meta name="description"        content={description}></meta>
             <meta property="og:type"        content="article" />
             <meta property="og:title"       content={title} />
             <meta property="og:description" content={description} />
-            <meta property="og:image"       content={image || "http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"} /> */}
+            <meta property="og:image"       content={image || "http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"} />
         </Head>
     )
 }
